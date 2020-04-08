bf4aa45f4a154f486059ec9bcd87b6ccb629947c
robsonataide <robsonataide@gmail.com>
2020-04-08 13:03:52 +0100

    1 file modified

--- array-util.ts
+++ array-util.ts
@@ -1,14 +1,15 @@
 export class ArrayUtil{
   
   static intersectionCebola(arrayA:any[], arrayB:any[]):any[]{
+    
     const args = Array.from(arguments).sort((a,b)=> a.length - b.length );
     const intersection = args[0].filter(a => args[1].includes(a));
     return intersection;
   }
 
   static intersectionEdgar(arrayA:any[], arrayB:any[]):any[]{
-    const args = Array.from(arguments).sort((a,b)=> a.length - b.length );
-    const intersection = args[0].filter(a => args[1].includes(a));
-    return intersection;
+    const hashMap = {}
+    arrayA.forEach(a => hashMap[a] = true)
+    return arrayB.filter(b => hashMap[b]);
   }
 }
