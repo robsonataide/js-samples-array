fd97d181ac3fcf15d28bfa0796abe6a1b9fa37e9
robsonataide <robsonataide@gmail.com>
2020-04-08 10:29:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-samples-array
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-samples-array)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import {ArrayUtil} from './array-util';
+
+let a = [1,2,3,4,5,6,7];
+let b = [1,4,6,8];
+
+console.log(ArrayUtil.intersection(a,b));
