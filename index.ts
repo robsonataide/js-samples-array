43703f546ef3f4922bbc7d584024266b214249c5
robsonataide <robsonataide@gmail.com>
2020-04-08 12:11:45 +0100

    Change for display in document

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ import {ArrayUtil} from './array-util';
 let a = [1,2,3,4,5,6,7];
 let b = [1,4,6,8];
 
-console.log(ArrayUtil.intersection(a,b));
+document.write(ArrayUtil.intersection(a,b).join(','));
