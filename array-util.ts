4b9a3455a79af4048e827d61dac02f017cfc2e26
robsonataide <robsonataide@gmail.com>
2020-04-08 15:14:11 +0100

    2 files modified

--- array-util.ts
+++ array-util.ts
@@ -1,15 +1,20 @@
 export class ArrayUtil{
   
-  static intersectionCebola(arrayA:any[], arrayB:any[]):any[]{
-    
+  static intersection(arrayA:any[], arrayB:any[]):any[]{
+    console.time('intersection o(n^2)')
     const args = Array.from(arguments).sort((a,b)=> a.length - b.length );
     const intersection = args[0].filter(a => args[1].includes(a));
+    console.timeEnd('intersection o(n^2)')
     return intersection;
   }
 
-  static intersectionEdgar(arrayA:any[], arrayB:any[]):any[]{
+  static intersectionFast(arrayA:any[], arrayB:any[]):any[]{
+    console.time('intersection o(n)')
     const hashMap = {}
     arrayA.forEach(a => hashMap[a] = true)
-    return arrayB.filter(b => hashMap[b]);
+    console.timeEnd('intersection o(n)')
+    const intersection = arrayB.filter(b => hashMap[b]);
+    return intersection;
   }
+  
 }
--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ let a = [1,2,3,4,5,6,7];
 let b = [1,4,6,8];
 
 document.write(ArrayUtil.intersection(a,b).join(','));
+document.write(ArrayUtil.intersectionFast(a,b).join(','));
