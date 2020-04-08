0bb12fdf82f36ede5595cacbb1216755e3e7c5b8
robsonataide <robsonataide@gmail.com>
2020-04-08 17:16:06 +0100

    3 files modified

--- array-util.ts
+++ array-util.ts
@@ -1,6 +1,6 @@
 export class ArrayUtil{
   
-  static intersection(arrayA:any[], arrayB:any[]):any[]{
+  static intersection1(arrayA:any[], arrayB:any[]):any[]{
     console.time('intersection 1');
     const args = Array.from(arguments).sort((a,b)=> a.length - b.length );
     const intersection = args[0].filter(a => args[1].includes(a));
@@ -8,11 +8,20 @@ export class ArrayUtil{
     return intersection;
   }
 
-  static intersectionFast(arrayA:any[], arrayB:any[]):any[]{
+  static intersection2(arrayA:any[], arrayB:any[]):any[]{
     console.time('intersection 2');
     const keys = new Set(arrayA);
     const intersection = arrayB.filter(b => keys.has(b));
     console.timeEnd('intersection 2');
+    return intersection;
+  }
+
+  static intersection3(arrayA:any[], arrayB:any[]):any[]{
+    console.time('intersection 3');
+    const keys = [];
+    arrayA.forEach(el => keys[el] = true );
+    const intersection = arrayB.filter(b => keys[b]);
+    console.timeEnd('intersection 3');
     return intersection;
   }
   
--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import {ArrayUtil} from './array-util';
 let a = "Uma nebulosa, também conhecida como nébula, é uma nuvem interestelar de poeira, hidrogênio, hélio e gases ionizados. Originalmente, nebulosa era o nome de qualquer corpo celeste difuso, incluindo galáxias além da Via Láctea. A Galáxia de Andrômeda, por exemplo, era atribuída como Nebulosa de Andrômeda (e galáxias espirais em geral como \"nebulosas espirais\") antes da verdadeira natureza das galáxias ser confirmada no início do século XX por Vesto Melvin Slipher, Edwin Hubble e outros.".split(' ');
 let b = "Nebulosas são muitas vezes regiões de formações estrelares, como a Nebulosa da Águia. Essa nebulosa é retratada em uma das imagens mais famosas da NASA, os \"Pilares da Criação\". Nessas regiões a formação de gás, poeira e outros materiais amontoam-se para formar massas maiores, nas quais atraem mais massas, e eventualmente se tornarão maciças o suficiente para se tornarem estrelas. Os materiais remanescentes são acreditados formarem planetas, e outros objetos de sistemas planetários.".split(' ');
 
-document.getElementById("first").append(ArrayUtil.intersection(a,b).join(','));
+document.getElementById("first").append(ArrayUtil.intersection1(a,b).join(','));
 
-document.getElementById("second").append(ArrayUtil.intersectionFast(a,b).join(','));
+document.getElementById("second").append(ArrayUtil.intersection2(a,b).join(','));
+
+document.getElementById("third").append(ArrayUtil.intersection3(a,b).join(','));
